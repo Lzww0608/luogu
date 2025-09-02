5428231c7e1b9ad51373fef3bbc8132a1b3ebd22
lzw <1094693070@qq.com>
2025-09-02 15:28:36 +0800

    Init

---
+++ luogu/P1000.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <cmath>
+#include <queue>
+#include <utility>
+#include <tuple>
+#include <ranges>
+#include <functional>
+#include <vector>
+#include <array>
+#include <random>
+#include <stack>
+#include <bitset>
+#include <deque>
+#include <ios>
+#include <list>
+
+
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	std::cout << "                ********" << std::endl;
+	std::cout << "               ************" << std::endl;
+	std::cout << "               ####....#." << std::endl;
+	std::cout << "             #..###.....##...." << std::endl;
+	std::cout << "             ###.......######              ###            ###" << std::endl;
+	std::cout << "                ...........               #...#          #...#" << std::endl;
+	std::cout << "               ##*#######                 #.#.#          #.#.#" << std::endl;
+	std::cout << "            ####*******######             #.#.#          #.#.#" << std::endl;
+	std::cout << "           ...#***.****.*###....          #...#          #...#" << std::endl;
+	std::cout << "           ....**********##.....           ###            ###" << std::endl;
+	std::cout << "           ....****    *****...." << std::endl;
+	std::cout << "             ####        ####" << std::endl;
+	std::cout << "           ######        ######" << std::endl;
+	std::cout << "##############################################################" << std::endl;
+	std::cout << "#...#......#.##...#......#.##...#......#.##------------------#" << std::endl;
+	std::cout << "###########################################------------------#" << std::endl;
+	std::cout << "#..#....#....##..#....#....##..#....#....#####################" << std::endl;
+	std::cout << "##########################################    #----------#" << std::endl;
+	std::cout << "#.....#......##.....#......##.....#......#    #----------#" << std::endl;
+	std::cout << "##########################################    #----------#" << std::endl;
+	std::cout << "#.#..#....#..##.#..#....#..##.#..#....#..#    #----------#" << std::endl;
+	std::cout << "##########################################    ############" << std::endl;
+}
---
+++ luogu/P1001.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <algorithm>
+
+
+int main() {
+	int a, b;
+	std::cin >> a >> b;
+	std::cout << a + b;
+}
